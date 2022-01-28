421deec78587dadce7fdb00f3d435e62e6c4e74c
garaiza-93 <araizagustavo93@gmail.com>
2022-01-28 17:22:25 +0000

    Added flag detection for options.

--- bcGen.sh
+++ bcGen.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+addCmd=0
+removeCmd=0
+updateCmd=0
+nSkus=1
+file=''
+
 function help() {
 	echo "	Usage: $0  [options]"
 	cat usage
 }
 
-if [ $# -eq 0 ] 
-then
-	help
-	exit 1
-else
-
-	exit 0
-fi
+while getopts 'harun:o:' flag; do
+	case "${flag}" in
+		h) help exit 1 ;;
+		a) addCmd=1 ;;
+		r) removeCmd=1 ;;
+		u) updateCmd=1 ;;
+		n) nSkus=$OPTARG ;;
+		o) file="$OPTARG" ;;
+	esac
+done
 
