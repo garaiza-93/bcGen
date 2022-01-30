04fb331b94854429876b4ff4a8db2f1479b4a514
garaiza-93 <araizagustavo93@gmail.com>
2022-01-29 18:47:08 -0600

    Still WIP.

--- bcGen.sh
+++ bcGen.sh
@@ -3,22 +3,37 @@
 addCmd=0
 removeCmd=0
 updateCmd=0
-nSkus=1
+lines=1
 file=''
-
+text=''
 function help() {
-	echo "	Usage: $0  [options]"
+	echo "	Usage: $0  [options] 'text to !! use'"
 	cat usage
 }
-
+#Process options
 while getopts 'harun:o:' flag; do
 	case "${flag}" in
 		h) help exit 1 ;;
-		a) addCmd=1 ;;
-		r) removeCmd=1 ;;
-		u) updateCmd=1 ;;
-		n) nSkus=$OPTARG ;;
+		a) addCmd=1 removeCmd=0 updateCmd=0;;
+		r) addCmd=0 removeCmd=1 updateCmd=0;;
+		u) addCmd=0 removeCmd=0 updateCmd=1 ;;
+		n) lines=$OPTARG ;;
 		o) file="$OPTARG" ;;
 	esac
 done
+if [ $(( addCmd^removeCmd^updateCmd )) -lt 1 ]; then
+	help
+	exit 1
+fi
+
+#check for text and process it
+if [ $(( $# - $OPTIND )) -lt 0 ]; then
+	help
+	exit 1
+else
+	text=${@:$OPTIND:1}
+	echo "Text: ${text}"
+	echo "Lines: ${lines}"
+	
+fi
 
